56a60d5544dad541d4499cb4c951d5cadf0dd85f
Maheel <Maheellakshanra@gmail.com>
2023-09-07 02:36:21 +0530

    basic funtionalities and chachin are done.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,79 @@ import React, { useEffect, useState } from 'react';
 import data from './data/cities.json';
 import { fetchWeatherData } from './services/apiServices'; // Import the API function
 import WeatherCard from './components/cards/weatherCard'; // Ensure proper casing
+import background from './assets/1 Dashboard - 1200px.png';
+import { Button } from '@mui/material';
+import './App.css';
 
 function App() {
+  const [selectedCard, setSelectedCard] = useState(null);
   const cityCodes = data.List.map((city) => city.CityCode);
 
   const [weatherData, setWeatherData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const weatherResults = await fetchWeatherData(cityCodes);
-      setWeatherData(weatherResults);
+  // Function to fetch and cache weather data
+  const fetchAndCacheWeatherData = async () => {
+    const cachedData = localStorage.getItem('weatherData');
+    if (cachedData) {
+      // Use cached data if it exists and is not expired
+      const parsedData = JSON.parse(cachedData);
+      const currentTime = new Date().getTime();
+      if (parsedData.timestamp && currentTime - parsedData.timestamp < 300000) {
+        setWeatherData(parsedData.data);
+        return;
+      }
     }
 
-    fetchData();
+    // Fetch fresh data if cache is expired or doesn't exist
+    const weatherResults = await fetchWeatherData(cityCodes);
+    setWeatherData(weatherResults);
+
+    // Cache the data with a timestamp
+    localStorage.setItem(
+      'weatherData',
+      JSON.stringify({ data: weatherResults, timestamp: new Date().getTime() })
+    );
+  };
+
+  useEffect(() => {
+    fetchAndCacheWeatherData();
   }, []);
 
+  const handleCardClick = (index) => {
+    setSelectedCard(index);
+  };
+
+  const handleBackClick = () => {
+    setSelectedCard(null);
+  };
+
   return (
-    <div className="App">
-      {weatherData.map((weatherInfo, index) => (
-        <WeatherCard key={index} weatherInfo={weatherInfo} />
-      ))}
+    <div className="app-container">
+      <div className="cards">
+        <div className="background-image">
+          <img src={background} alt="Background" />
+        </div>
+        <div className="weather-cards">
+          {weatherData.map((weatherInfo, index) => (
+            <WeatherCard
+              key={index}
+              weatherInfo={weatherInfo}
+              isSelected={selectedCard === index}
+              onClick={() => handleCardClick(index)}
+            />
+          ))}
+          {selectedCard !== null && (
+            <div className="back-button">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleBackClick}
+              >
+                Back
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/cards/weatherCard.js
+++ src/components/cards/weatherCard.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  CardActionArea,
+  Button,
+} from '@mui/material';
+import './weatherCard.css';
 
-function WeatherCard({ weatherInfo }) {
-  const { name, main, weather } = weatherInfo;
+function WeatherCard({ weatherInfo, isSelected, onClick }) {
+  const { name, main, weather, dt, id } = weatherInfo;
+  const description = weather[0].description;
+  const temp = main.temp;
+  const timestamp = dt;
+
+  const date = new Date(timestamp * 1000);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = date.toLocaleDateString(undefined, options);
+  const formattedTime = date.toLocaleTimeString();
+
+  const weatherImageMapping = {
+    'clear sky': 'Clear_Sky.png',
+    mist: 'Mist.png',
+    'few clouds': 'Few_Clouds.png',
+    'broken clouds': 'Broken_Clouds.png',
+    'overcast clouds': 'Light_Ran.png',
+  };
+
+  const weatherImage = weatherImageMapping[description] || 'Mist.png';
 
   return (
-    <div className="weather-card">
-      <h2>{name}</h2>
-      <p>Temperature: {main.temp}°C</p>
-      <p>Description: {weather[0].description}</p>
-    </div>
+    <Card
+      className={`weather-card ${isSelected ? 'selected' : ''}`}
+      onClick={onClick}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Weather Icon"
+          height="100"
+          image={require(`../../assets/${weatherImage}`)}
+        />
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Description: {description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Temperature: {temp}°C
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Last Update: {formattedDate} at {formattedTime}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            ID: {id}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
 
